1b414813fca16bfea1bcfae3b301e004c3b9a914
Edgar Cervantes <edgar.cervantes@vallartasupermarkets.com>
2024-01-29 15:22:39 -0800

    FirstPhaseComplete

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/*==================== MENU SHOW & HIDE ====================*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+/*===== Menu Show =====*/
+/* Validate If Constant Exists*/
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+/*==================== REMOVE MENU MOBILE ====================*/
+
+/*==================== ACCORDIAN SKILLS ====================*/
+
+/*==================== QUALIFICATION TABS ====================*/
+
+//24:39 https://www.youtube.com/watch?v=27JtRAI3QO8&t=357s
