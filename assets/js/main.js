9a7db3f12a06f07beb804e923870051a86524588
Edgar Cervantes <72948345+Cervzz@users.noreply.github.com>
2024-01-29 21:38:55 -0800

    Added images. Structured CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,31 @@ if(navToggle){
         navMenu.classList.add('show-menu')
     })
 }
+/*===== Menu Hide =====*/
+/* Validate If Constant Exists*/
+if(navClose){
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDIAN SKILLS ====================*/
 
 /*==================== QUALIFICATION TABS ====================*/
+
+/*==================== SERVICES MODAL ====================*/
+
+/*==================== PORTFOLIO SWIPER ====================*/
+
+/*==================== TESTIMONIALS ====================*/
 
 //24:39 https://www.youtube.com/watch?v=27JtRAI3QO8&t=357s
